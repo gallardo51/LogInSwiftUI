26463577ca96f92d5d6f4170623ccd009d6aeb56
Alex Sobolev <31108@mail.ru>
2024-04-26 13:49:54 +0300

    Setup ButtonAppearanceModifier

---
+++ LogInSwiftUI/ButtonAppearanceModifier.swift
@@ -0,0 +1,30 @@
+//
+//  ButtonAppearanceModifier.swift
+//  LogInSwiftUI
+//
+//  Created by Александр Соболев on 26.04.2024.
+//
+
+import SwiftUI
+
+struct ButtonAppearanceModifier: ViewModifier {
+    
+    let color: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 200, height: 60)
+            .background(color)
+            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.black, lineWidth: 4)
+            )
+    }
+}
+
+extension View {
+    func buttonAppearance(_ color: Color) -> some View {
+        ModifiedContent(
+            content: self,
+            modifier: ButtonAppearanceModifier(color: color)
+        )
+    }
+}
