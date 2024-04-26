171e08ebee622212d5328fd5c4cb24841b391810
Alex Sobolev <31108@mail.ru>
2024-04-26 13:25:35 +0300

    Initial Commit

---
+++ LogInSwiftUI/LogInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogInSwiftUIApp.swift
+//  LogInSwiftUI
+//
+//  Created by Александр Соболев on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LogInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
